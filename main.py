1348a32dc08f09792832cc0a290c9aff873dd2a7
Jordan M <jmcclusky17@gmail.com>
2018-01-13 01:19:38 -0600

    second commit, alot of groundwork

---
+++ main.py
@@ -0,0 +1,51 @@
+from flask import Flask, request, redirect, render_template
+from flask_sqlalchemy import SQLAlchemy 
+
+app = Flask(__name__)
+app.config['DEBUG'] = True
+# Note: the connection string after :// contains the following info:
+# user:password@server:portNumber/databaseName
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:1234@localhost:8889/build-a-blog'
+app.config['SQLALCHEMY_ECHO'] = True
+db = SQLAlchemy(app)
+
+
+class Blog(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(120))
+    body = db.Column(db.String(120))
+
+    def __init__(self, title, body):
+        self.title = title
+        self.body = body
+
+tasks = []
+@app.route('/', methods=['POST', 'GET'])
+def index():
+    
+    testblock = Blog.query.first()
+    blog_titles = "hello"
+    #bodys = 
+    return render_template('blog.html',title="Build A Blog!", testblock=testblock)
+
+
+@app.route('/newpost', methods=['POST', 'GET'])
+def newpost():
+    # TODO - If either the blog title or blog body is left empty in the new post form, 
+    #           the form is rendered again, with a helpful error message and 
+    #           any previously-entered content in the same form inputs
+    if request.method == 'POST':
+        title = request.form['title']
+        body = request.form['body']
+        new_blog = Blog(title, body)
+        db.session.add(new_blog)
+        db.session.commit()
+        
+        return redirect('/')
+    return render_template('newpost.html', title="Build A Blog!")
+
+
+
+if __name__ == '__main__':
+    app.run()
